4395e3a3e7e8745fa7b1df8ea6519a597e97ee34
dheeraj <dheerajverma2903@gmail.com>
2026-08-03 20:32:19 +0530

    cart-server-db-auth-implemented

--- Microservices/cart/src/app.js
+++ Microservices/cart/src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cartRouter = require('./routes/cart.routes')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/cart', cartRouter)
 
 module.exports = app;
