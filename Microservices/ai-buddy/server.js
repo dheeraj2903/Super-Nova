8015d89b9c279342ba640e07f8e7505d44f0b0ca
dheeraj <dheerajverma2903@gmail.com>
2026-08-21 21:51:33 +0530

    fixes-in-ai-buddy

--- Microservices/ai-buddy/server.js
+++ Microservices/ai-buddy/server.js
@@ -1,17 +1,3 @@
-// require('dotenv').config()
-// const app = require('./src/app');
-// const http = require('http');
-
-// const { initSocketServer } = require('./src/sockets/socket.server');
-
-// const httpServer = http.createServer(app);
-
-// initSocketServer(httpServer)
-
-// httpServer.listen(3005, () => {
-//     console.log("AI Buddy server is running on port 3005")
-// })
-
 require('dotenv').config()
 const app = require('./src/app');
 const http = require('http');
